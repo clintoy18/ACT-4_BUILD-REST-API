a181a84b3fa18eb090c7522dab0dca6406f7cc4e
clintoy18 <remartybanez30@gmail.com?>
2025-02-19 12:50:34 +0800

    products data

--- src/products/product.routes.ts
+++ src/products/product.routes.ts
@@ -5,12 +5,17 @@ import {StatusCodes} from "http-status-codes"
 
 export const productRouter = express.Router()
 
-productRouter.get("/products", async (req: Request,res : Response) =>{
+//finds all the products in the file storage and returns
+
+productRouter.get("/products", async (req: Request,res : Response):Promise<any> =>{
     try{
         const allProducts = await database.findAll()
 
         if(!allProducts){
             return res.status(StatusCodes.NOT_FOUND).json({msg: `No products found at this time...`})
         }
+        return res.status(StatusCodes.OK).json({totalProducts: allProducts.length, allProducts})
+    }catch(error){
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
     }
 })
